0196f6dd6da37ded941bb3677f5c72b9b6ec6bb2
kaevtimov <39729854+kaevtimov@users.noreply.github.com>
2018-11-11 12:39:07 +0200

    fixed rent service base

--- src/main/java/com/evtimov/landlordapp/backend/services/base/RentService.java
+++ src/main/java/com/evtimov/landlordapp/backend/services/base/RentService.java
@@ -4,12 +4,16 @@ import com.evtimov.landlordapp.backend.models.Rent;
 
 public interface RentService {
 
-    Rent addRent(Rent rent);
+    Rent addNextRent(Rent rent);
+
+    Rent addFirstRent(Rent rent);
 
     Rent updateRentRemaining(int rentId, Rent rent);
 
     Rent updateRentIsPaidStatus(int rentId);
 
     Rent getRentByPlaceId(int placeId);
+
+    Rent editRent(Rent rent, int rentId);
 
 }
