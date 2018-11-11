f00f5d293c91fbd729e42708cf42df804f29489c
kaevtimov <39729854+kaevtimov@users.noreply.github.com>
2018-11-11 12:27:07 +0200

    fixed message model

--- src/main/java/com/evtimov/landlordapp/backend/models/Message.java
+++ src/main/java/com/evtimov/landlordapp/backend/models/Message.java
@@ -19,88 +19,42 @@ public class Message {
     private int messageID;
 
     @Column(name = "timestamp")
-    @NotNull(message = "Date cannot be null!")
-    private Date timestamp;
+    private String timestamp;
 
-<<<<<<< HEAD
-    @Column(name = "sender_token")
-    private String senderRegistrationToken;
-
-    @Column(name = "receiver_token")
-    private String receiverToken;
-
-    /*@Column(name = "landlordId")
-    private int landlordID;
-*/
-    /*@Column(name = "chatsessionId")
-=======
     @Column(name = "senderId")
-    @Min(value = 1, message = "Sender ID must be at least 1!")
     @Max(value = 2147483647, message = "Sender ID is too big!")
     @NotNull(message = "Sender ID cannot be null!")
     private int senderID;
 
     @Column(name = "receiverId")
-    @Min(value = 1, message = "Receiver ID must be at least 1!")
     @Max(value = 2147483647, message = "Receiver ID is too big!")
     @NotNull(message = "Receiver ID cannot be null!")
     private int receiverID;
 
     @Column(name = "chatsessionId")
-    @Min(value = 1, message = "ChatSession ID must be at least 1!")
     @Max(value = 2147483647, message = "ChatSession ID is too big!")
     @NotNull(message = "ChatSession ID cannot be null!")
->>>>>>> 5b466dffa9d1220212ba3c19f1bf0f0a333530e4
-    private int chatSessionID;
-*/
+    private int chatsessionId;
+
     @Column(name = "text")
     @NotNull(message = "Text message cannot be null! Please enter something!")
     @Size(min = 1, max = 16777215, message = "Enter a message with length between 1 and 16777215!")
     private String text;
 
-    @Column(name = "picture")
-    private String picture;
-
-<<<<<<< HEAD
-/*    @Column(name = "isdelivered")
-    private boolean isDelivered;
-
-    @Column(name = "archive")
-    private boolean isArchived;*/
-=======
-    @Column(name = "isdelivered")
-    @NotNull(message = "Is delivered cannot be null!")
-    private boolean isDelivered;
-
-    @Column(name = "archive")
-    @NotNull(message = "Is archived cannot be null!")
-    private boolean isArchived;
->>>>>>> 5b466dffa9d1220212ba3c19f1bf0f0a333530e4
 
 
     public Message() {
         //default
     }
 
-    public Message(Date timestamp, String text,
-<<<<<<< HEAD
-                   String picture, int tenantID, int landlordID, boolean isDelivered, int chatSessionId, boolean isArchived, String senderRegistrationToken, String receiverToken) {
-        this.receiverToken = receiverToken;
-        setSenderRegistrationToken(senderRegistrationToken);
-        /*setArchived(isArchived);
-=======
-                   String picture, int senderID, int receiverID, boolean isDelivered, int chatSessionId, boolean isArchived) {
-        setArchived(isArchived);
->>>>>>> 5b466dffa9d1220212ba3c19f1bf0f0a333530e4
-        setChatSessionId(chatSessionId);
+    public Message(String timestamp, String text, int senderID, int receiverID, int chatSessionId) {
+        setChatsessionId(chatSessionId);
         setSenderID(senderID);
         setReceiverID(receiverID);
-        setIsDelivered(isDelivered);
-        */
         setTimestamp(timestamp);
         setText(text);
-        setPicture(picture);
     }
+
 
     public int getMessageID() {
         return messageID;
@@ -110,57 +64,19 @@ public class Message {
         this.messageID = messageID;
     }
 
-
-    public Date getTimestamp() {
+    public String getTimestamp() {
         return timestamp;
     }
 
-    private void setTimestamp(Date timestamp) {
+    public void setTimestamp(String timestamp) {
         this.timestamp = timestamp;
     }
 
-    public String getText() {
-        return text;
-    }
-
-    private void setText(String text) {
-        this.text = text;
-    }
-
-    public String getPicture() {
-        return picture;
-    }
-
-    private void setPicture(String picture) {
-        this.picture = picture;
-    }
-
-<<<<<<< HEAD
-    public String getSenderRegistrationToken() {
-        return senderRegistrationToken;
-    }
-
-    public void setSenderRegistrationToken(String senderRegistrationToken) {
-        this.senderRegistrationToken = senderRegistrationToken;
-    }
-
-    public String getReceiverToken() {
-        return receiverToken;
-    }
-
-    public void setReceiverToken(String receiverToken) {
-        this.receiverToken = receiverToken;
-    }
-
-  /*  public int getTenantID() {
-        return tenantID;
-=======
     public int getSenderID() {
         return senderID;
->>>>>>> 5b466dffa9d1220212ba3c19f1bf0f0a333530e4
     }
 
-    private void setSenderID(int senderID) {
+    public void setSenderID(int senderID) {
         this.senderID = senderID;
     }
 
@@ -168,31 +84,23 @@ public class Message {
         return receiverID;
     }
 
-    private void setReceiverID(int receiverID) {
+    public void setReceiverID(int receiverID) {
         this.receiverID = receiverID;
     }
 
-    public boolean getIsDelivered() {
-        return isDelivered;
+    public int getChatsessionId() {
+        return chatsessionId;
     }
 
-    private void setIsDelivered(boolean isDelivered) {
-        this.isDelivered = isDelivered;
+    public void setChatsessionId(int chatsessionId) {
+        this.chatsessionId = chatsessionId;
     }
 
-    public int getChatSessionId() {
-        return chatSessionID;
+    public String getText() {
+        return text;
     }
 
-    private void setChatSessionId(int chatSessionId) {
-        this.chatSessionID = chatSessionId;
+    public void setText(String text) {
+        this.text = text;
     }
-
-    public boolean isArchived() {
-        return isArchived;
-    }
-
-    private void setArchived(boolean archived) {
-        isArchived = archived;
-    }*/
 }
