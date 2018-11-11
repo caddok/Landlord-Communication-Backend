e696f3c5c277d94de544d7514eff7df114c384da
kaevtimov <39729854+kaevtimov@users.noreply.github.com>
2018-11-11 12:25:28 +0200

    fixed user controller

--- src/main/java/com/evtimov/landlordapp/backend/controllers/UserController.java
+++ src/main/java/com/evtimov/landlordapp/backend/controllers/UserController.java
@@ -3,22 +3,17 @@ package com.evtimov.landlordapp.backend.controllers;
 import com.evtimov.landlordapp.backend.models.User;
 import com.evtimov.landlordapp.backend.services.base.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
-
 @RestController
 @RequestMapping("/api/users")
-<<<<<<< HEAD
-public class UserController {                    //CHECKED
-=======
 public class UserController {
 
->>>>>>> 5b466dffa9d1220212ba3c19f1bf0f0a333530e4
     private final UserService service;
+
 
     @Autowired
     public UserController(UserService service) {
@@ -27,21 +22,8 @@ public class UserController {
 
 
     @RequestMapping(method = RequestMethod.POST)
-<<<<<<< HEAD
-    public User registerUser(@RequestBody User user){
-        service.registerUser(user);
-        return user;
-=======
     public User registerUser(@RequestBody @Valid User user){
         return service.registerUser(user);
->>>>>>> 5b466dffa9d1220212ba3c19f1bf0f0a333530e4
-    }
-
-    @RequestMapping(value = "/{username}",method = RequestMethod.POST)
-    public ResponseEntity<String> getRegistrationToken(@PathVariable String username, String token) {
-        User user = service.findUserByUsername(username);
-        service.updateUserRegistrationToken(user.getUserID(),token);
-        return ResponseEntity.ok(token);
     }
 
     @RequestMapping(value = "/tenants", method = RequestMethod.GET)
@@ -49,7 +31,7 @@ public class UserController {
         return service.findAllTenants();
     }
 
-    @RequestMapping(/*value = "/landlords", */method = RequestMethod.GET)
+    @RequestMapping(value = "/landlords", method = RequestMethod.GET)
     public List<User> getAllLandlords(){
         return service.findAllLandlords();
     }
