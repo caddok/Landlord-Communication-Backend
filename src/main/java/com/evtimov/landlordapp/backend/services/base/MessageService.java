d9e30d79e0f39a402e37d99414385014506db958
kaevtimov <39729854+kaevtimov@users.noreply.github.com>
2018-11-11 12:37:36 +0200

    fixed message service base

--- src/main/java/com/evtimov/landlordapp/backend/services/base/MessageService.java
+++ src/main/java/com/evtimov/landlordapp/backend/services/base/MessageService.java
@@ -8,20 +8,10 @@ public interface MessageService {
 
     Message createMessage(Message message);
 
-<<<<<<< HEAD
-    void sendMessage(String receiverToken);
-
-    /*List<Message> getAllDeliveredMessagesByTenantId(int tenantId);
-
-    List<Message> getAllUndeliveredMessagesByTenantId(int tenantId);
-
-    List<Message> getAllDeliveredMessagesByLandlordId(int landlordId);
-
-    List<Message> getAllUndeliveredMessagesByLandlordId(int landlordId);*/
-=======
     List<Message> getMessagesBySenderIdAndChatId(int senderId, int chatId);
 
     List<Message> getMessagesByReceiverIdAndChatId(int receiverId, int chatId);
->>>>>>> 5b466dffa9d1220212ba3c19f1bf0f0a333530e4
+
+    List<Message> getMessagesByChatId(int chatId);
 
 }
