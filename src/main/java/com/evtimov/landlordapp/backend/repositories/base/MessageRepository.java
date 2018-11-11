67cd5a0a37167a0463770ecd82f9e1b07b7f7b95
kaevtimov <39729854+kaevtimov@users.noreply.github.com>
2018-11-11 12:30:42 +0200

    fixed message repo base

--- src/main/java/com/evtimov/landlordapp/backend/repositories/base/MessageRepository.java
+++ src/main/java/com/evtimov/landlordapp/backend/repositories/base/MessageRepository.java
@@ -8,17 +8,9 @@ public interface MessageRepository {
 
     Message createMessage(Message message);
 
-<<<<<<< HEAD
-   /* List<Message> getAllDeliveredMessagesByTenantId(int tenantId);
-
-    List<Message> getAllUndeliveredMessagesByTenantId(int tenantId);
-
-    List<Message> getAllDeliveredMessagesByLandlordId(int landlordId);
-
-    List<Message> getAllUndeliveredMessagesByLandlordId(int landlordId);*/
-=======
     List<Message> getMessagesBySenderIdAndChatId(int senderId, int chatId);
 
     List<Message> getMessagesByReceiverIdAndChatId(int receiverId, int chatId);
->>>>>>> 5b466dffa9d1220212ba3c19f1bf0f0a333530e4
+
+    List<Message> getMessagesByChatId(int chatId);
 }
